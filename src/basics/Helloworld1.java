349d55b0baae28c2c8ea57b7677ed20842b8f060
sadhana <sadhu.lokii1924@gmail.com>
2023-05-10 14:22:15 +0530

    added comments

--- src/basics/Helloworld1.java
+++ src/basics/Helloworld1.java
@@ -1,10 +1,9 @@
 package basics;
 //first java program 1
-//git and git hub
 public class Helloworld1 {
 	public static void main(String args[])
 	{
-		System.out.println("hello world");
+		System.out.println("hello sadhana");
 	}
 
 }
