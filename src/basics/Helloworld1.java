6c5a455bf2bf66c33bf33edbbd9f0398a47d85db
sadhana <sadhu.lokii1924@gmail.com>
2023-05-10 14:35:07 +0530

    added comments

--- src/basics/Helloworld1.java
+++ src/basics/Helloworld1.java
@@ -4,7 +4,7 @@ public class Helloworld1 {
 	public static void main(String args[])
 	{
 		System.out.println("hello world");
-		System.out.println("hello suba sadhana");
+		System.out.println("hello sadhana");
 	}
 
 }
