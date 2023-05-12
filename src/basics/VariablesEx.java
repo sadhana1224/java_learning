a381f84c86b47d2a1aebb12e96a7dec0b645b53a
sadhana <sadhu.lokii1924@gmail.com>
2023-05-12 12:39:49 +0530

    varibales example source code

---
+++ src/basics/VariablesEx.java
@@ -0,0 +1,28 @@
+package basics;
+
+public class VariablesEx {
+	public static void main(String args[])
+	{
+		//camelcasing (variable names, methodnames)
+          int ticketPrice;
+          
+          double pi=3.14;
+          
+          short SNo=1;
+          
+          char gender='M';
+          
+          String name="sadhana";
+          
+          ticketPrice=9000;
+          
+          System.out.println(ticketPrice);
+          
+          System.out.println("The price of the ticket is "+ticketPrice);
+          System.out.println("Value of pi is  "+pi);
+          System.out.println("Serial number is "+SNo);
+          System.out.println("The gender is  "+gender);
+          System.out.println("The name is  "+name);
+	}
+
+}
