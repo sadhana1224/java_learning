6f249ea3a715377ea417d48525616084b4896689
sadhana <sadhu.lokii1924@gmail.com>
2023-05-15 13:06:55 +0530

    assignment operator source code

---
+++ src/basics/AssignmentOperator.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int num1=5;
+		num1+=5;
+		
+		System.out.println("+=  ---> "+num1);
+		int num2=4;
+		num2-=2;
+		System.out.println("-=  ---> "+num2);
+		int num3=10;
+		num3*=10;
+		System.out.println("*=  --> "+num3);
+		int num4=10;
+		num4/=2;
+		System.out.println("/=  --> "+num4);
+		int num5=5;
+		num5%=5;
+		System.out.println("%=  --> "+num5);
+		
+
+	}
+
+}
