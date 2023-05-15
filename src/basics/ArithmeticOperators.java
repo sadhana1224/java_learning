537da81cf0bdb2e68bf76c7788b3c34a5df502bd
sadhana <sadhu.lokii1924@gmail.com>
2023-05-15 12:04:07 +0530

    operators source code

---
+++ src/basics/ArithmeticOperators.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		int a=50, b=6;
+		
+		float c=50.0f;
+		float d=6.0f;
+		float res=(c/d);
+
+		System.out.println("Addition of two number is "+ (a+b));
+		System.out.println("Subtraction of two number is "+(a-b));
+		System.out.println("Multiplication of two number is "+(a*b));
+		System.out.println("Division of two number is "+res);
+		System.out.println("modulo of two number is "+(a%b));
+
+	}
+
+}
