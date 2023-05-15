8eb1e3ac1222773914f62c1edcd6393eee2e54c4
sadhana <sadhu.lokii1924@gmail.com>
2023-05-15 12:20:24 +0530

    Relationaloperators source code

---
+++ src/basics/RelationalOperators.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+	 int a=10, b=15;
+	 System.out.println("a > b is --> "+(a>b));
+	 System.out.println("a < b is --> "+(a<b));
+	 System.out.println("a <= b is --> "+(a<=b));
+	 System.out.println("a >= b is --> "+(a>=b));
+	 System.out.println("a == b is --> "+(a==b));
+	 System.out.println("a != b is --> "+(a!=b));
+
+	}
+
+}
