5605a87bac1344218bb79ca8727b90c151204b1a
sadhana <sadhu.lokii1924@gmail.com>
2023-05-15 11:40:14 +0530

    loop source code

---
+++ src/basics/IFElseifEx.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class IFElseifEx {
+	public static void main(String args[])
+	{
+		int a=15;
+		int b=15;
+		if(a>b)
+		{
+			System.out.println("A is greater than B");
+		}
+		else if(b>a)
+		{
+			System.out.println("B is greater than A");
+		}
+		else
+		{
+			System.out.println("A and B is equal");
+		}
+	}
+
+}
