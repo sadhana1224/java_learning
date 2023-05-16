6f2eef02e770a685d3cff2cbddbfced73241b001
sadhana <sadhu.lokii1924@gmail.com>
2023-05-16 10:17:46 +0530

    factorial source code

---
+++ src/assingment/FactorialEx.java
@@ -0,0 +1,17 @@
+package assingment;
+
+public class FactorialEx {
+
+	public static void main(String[] args) {
+
+		int factorialNumber=5;
+		int factorialResult=1;
+		for(int i=1;i<=factorialNumber;i++)
+		{
+			factorialResult=factorialResult*i;
+			//System.out.println("factorial of a number "+ i  +" is :"+factorialResult); // this is individual i result
+		}
+		System.out.println("factorial of a number "+ factorialNumber +" is :"+factorialResult);
+	}
+
+}
