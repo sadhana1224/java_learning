634f8191750afd0cd6bbb9661ada703a350322e1
sadhana <sadhu.lokii1924@gmail.com>
2023-05-16 10:08:56 +0530

    for loop source code

---
+++ src/basics/ForloopEx.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class ForloopEx {
+
+	public static void main(String[] args) {
+     /*for(int i=1;i<=10;i++)
+     {
+    	 System.out.println(i);  //increment
+     }*/
+	
+	 for(int i=10;i>=1;i--)
+     {
+    	 System.out.println(i); //decrement
+     }
+     
+	}
+
+}
