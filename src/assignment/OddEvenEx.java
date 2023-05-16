96acc19174a57f2c5b09d8176a2b17c2e600bf37
sadhana <sadhu.lokii1924@gmail.com>
2023-05-16 10:43:09 +0530

    oddeven source code

---
+++ src/assignment/OddEvenEx.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class OddEvenEx {
+
+	public static void main(String[] args) {
+		for(int number=1;number<=5;number++)
+		{
+			if(number%2==0)
+			{
+
+				System.out.println("this is an even number :" +number);
+			}
+			else if(number%2==1)
+			{
+				System.out.println("this is an odd number  :" +number);
+			}
+		}
+
+	}
+
+}
