427f8e1a36bfc0b65b5f66daf084a54eb5f443e1
sadhana <sadhu.lokii1924@gmail.com>
2023-05-16 10:56:05 +0530

    perimeter of rectangle source code

---
+++ src/assignment/PerimeterofRectangle.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class PerimeterofRectangle {
+
+	public static void main(String[] args) {
+           int perimeter;
+           int length=25;
+           int width=4;
+           perimeter= 2*(length+width);
+           
+           System.out.println("Perimeter of rectangle is : "+ perimeter);
+	}
+
+}
